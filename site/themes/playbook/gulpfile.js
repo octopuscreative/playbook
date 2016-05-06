6deb3af1ac4ca3e9fc354e7fa5dfac4a26e05f17
Chris Griffith <chris@octopuscreative.com>
2016-05-06 13:23:35 -0700

    Add statamic

---
+++ site/themes/playbook/gulpfile.js
@@ -0,0 +1,28 @@
+var elixir = require('laravel-elixir');
+var theme = 'ink';
+elixir.config.assetsPath = './';
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Statamic theme. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(function(mix) {
+  mix
+    .sass(theme + '.scss', 'css/' + theme + '.css')
+    .coffee(['./coffee/**/*.coffee'], 'js/compiled/' + theme + '.js')
+    .scripts([
+      // 'vendor/modernizr.js',
+      // 'vendor/jquery-2.2.3.min.js',
+      // 'vendor/clipboard.js',
+      'compiled/' + theme + '.js'
+    ], 
+      'js/' + theme + '.js'
+    )
+});

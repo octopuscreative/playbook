8c19faccb324704b12b60e2f2dc2b1c2bb2978ea
Chris Griffith <chris@octopuscreative.com>
2016-07-13 11:31:02 -0700

    Adds general settings to source

---
+++ craft/config/general.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here.
+ * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
+ */
+
+return array(
+
+  '*' => array(
+    'omitScriptNameInUrls' => true,
+  ),
+
+  '.dev' => array(
+    'siteUrl' => 'playbook.octopus.dev',
+  ),
+
+  '.us' => array(
+    'siteUrl' => 'playbook.octop.us',
+  ),
+
+);
